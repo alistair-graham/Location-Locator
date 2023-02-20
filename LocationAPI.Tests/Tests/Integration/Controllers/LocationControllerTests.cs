6e6fc4d666ad5a4d0edf9f591567019147463990
Annette <annette.graham@sky.uk>
2023-02-20 03:30:40 +0000

    [Alistair] Add E2E tests for bad request responses

--- LocationAPI.Tests/Tests/Integration/Controllers/LocationControllerTests.cs
+++ LocationAPI.Tests/Tests/Integration/Controllers/LocationControllerTests.cs
@@ -1,4 +1,4 @@
-using Location_Locator.Models;
+﻿using Location_Locator.Models;
 using Location_Locator.Services.LocationService;
 using LocationAPI.Tests.Tests.Integration.Helpers;
 using Microsoft.AspNetCore.Builder;
@@ -56,6 +56,30 @@ namespace LocationAPI.Tests.Tests.Integration.Controllers
             Assert.Equal("London", locationResponse.City);
         }
 
+        [Theory]
+        [InlineData(PRIVATE_IP_ADDRESS, "private range")]
+        [InlineData(RESERVED_IP_ADDRESS, "reserved range")]
+        public async void Get_InvalidIPAddress_ReturnsBadResponse(string ipAddress, string errorMessage)
+        {
+            var factoryWithFakes = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.UseSetting("https_port", "5081");
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IStartupFilter>(new CustomRemoteIpStartupFilter(IPAddress.Parse(ipAddress)));
+                    services.AddHttpClient<ILocationService, StubLocationService>();
+                });
+            });
+
+            var client = factoryWithFakes.CreateClient();
+            var response = await client.GetAsync("api/location");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+            var responseMessage = await response.Content.ReadAsStringAsync();
+            Assert.Equal($"[\"The IP address <{ipAddress}> provided in the request must be public but was of type <{errorMessage}>\"]", responseMessage);
+        }
 
         [Fact]
         public async void Get_WithoutIPAddress_ReturnsBadResponse()
@@ -67,6 +91,43 @@ namespace LocationAPI.Tests.Tests.Integration.Controllers
 
             var responseMessage = await response.Content.ReadAsStringAsync();
             Assert.Equal("Failed to extract IP Address from the request.", responseMessage);
+        }
+
+        private class StubLocationService : ILocationService
+        {
+            public StubLocationService(HttpClient httpClient, ILogger<LocationService> logger) { }
+
+            #pragma warning disable CS1998 // Async method lacks 'await' because it is a stub
+            public async Task<Location> GetFromIPAddress(IPAddress ipAddress)
+            {
+                if (ipAddress.ToString() == PRIVATE_IP_ADDRESS)
+                {
+                    return new Location
+                    {
+                        Errors = new List<string> {
+                            $"The IP address <{PRIVATE_IP_ADDRESS}> provided in the request must be public but was of type <private range>"
+                        }
+                    };
+                }
+                else if (ipAddress.ToString() == RESERVED_IP_ADDRESS)
+                {
+                    return new Location
+                    {
+                        Errors = new List<string> {
+                            $"The IP address <{RESERVED_IP_ADDRESS}> provided in the request must be public but was of type <reserved range>"
+                        }
+                    };
+
+                }
+
+                return new Location
+                {
+                    IPAddress = ipAddress.ToString(),
+                    Country = "United Kingdom",
+                    RegionName = "England",
+                    City = "London",
+                };
+            }
         }
     }
 }
