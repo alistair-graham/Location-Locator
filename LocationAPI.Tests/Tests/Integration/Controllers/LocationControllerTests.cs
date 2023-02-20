5f77f34b4098edd79ecd68f640ce2a4ddb0963d4
Annette <annette.graham@sky.uk>
2023-02-20 03:23:54 +0000

    [Alistair] Added E2E test for happy path

---
+++ LocationAPI.Tests/Tests/Integration/Controllers/LocationControllerTests.cs
@@ -0,0 +1,60 @@
+using Location_Locator.Models;
+using Location_Locator.Services.LocationService;
+using LocationAPI.Tests.Tests.Integration.Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Xunit;
+using Xunit.Sdk;
+
+namespace LocationAPI.Tests.Tests.Integration.Controllers
+{
+    public class LocationControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+
+        private readonly WebApplicationFactory<Program> _factory;
+        private const string PRIVATE_IP_ADDRESS = "172.16.0.0";
+        private const string RESERVED_IP_ADDRESS = "127.0.0.1";
+
+        public LocationControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async void Get_WithValidIPAddress_ReturnsSuccessResponse()
+        {
+            var factoryWithFakes = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.UseSetting("https_port", "5081");
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IStartupFilter>(new CustomRemoteIpStartupFilter(IPAddress.Parse("24.48.0.1")));
+                    services.AddHttpClient<ILocationService, StubLocationService>();
+                });
+            });
+
+            var client = factoryWithFakes.CreateClient();
+            var response = await client.GetAsync("api/location");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+            var locationResponse = await response.Content.ReadFromJsonAsync<Location>();
+
+            Assert.NotNull(locationResponse);
+            Assert.Equal("24.48.0.1", locationResponse.IPAddress);
+            Assert.Equal("United Kingdom", locationResponse.Country);
+            Assert.Equal("England", locationResponse.RegionName);
+            Assert.Equal("London", locationResponse.City);
+        }
+
+    }
+}
